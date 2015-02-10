f0c207d215980628f457b49653b29853dce3b882
Connor T Cleary <connorcleary@gmail.com>
2015-02-10 09:52:23 -0800

    Refactor so we have one namespace object Colore, and only the ColoreColor and ColoreAdjuster have prototypes for more performant multiple instances.

--- colore.js
+++ colore.js
@@ -21,16 +21,11 @@
  *
  */
 
-var Colore = function(options) {
-  // Utilities
-  this.Mixer = new ColoreMixer();
-  this._white = new ColoreColor({
-    vals: { r:255, g:255, b:255 }
-  });
-  this._black = new ColoreColor({
-    vals: { r:0, g:0, b:0 }
-  });
-  
+
+
+var ColoreAdjuster = function(options) {
+  this.Utils = Colore.Utils;
+
   // Set defaults.
   this._baseColor = new ColoreColor();
   this._threshold = 600;
@@ -48,8 +43,7 @@ var Colore = function(options) {
     }
   }
 };
-Colore.prototype = {
-  Mixer: new ColoreMixer(),
+ColoreAdjuster.prototype = {
   baseColor: function(opt_newColor) {
     if (arguments.length) {
       this._baseColor = new ColoreColor({color: opt_newColor});
@@ -81,85 +75,142 @@ Colore.prototype = {
     }
   },
   tintColor: function(percentage) {
-    return this._shiftColor(percentage, true);
+    return this.Utils.shiftColor(this._baseColor, percentage, this.mixPure(), true);
   },
   shadeColor: function(percentage) {
-    return this._shiftColor(percentage, false);
-  },
-  _shiftColor: function(percentage, doTint) {
-    var weight;
-    if (typeof percentage === 'undefined') {
-      weight = 25; // Default of 25% weight
-    } else {
-      weight = (typeof percentage === 'string') ? parseInt(percentage, 10) : percentage;
-    }
-
-    var mixColor = this._determineMixColor(doTint);
-    var newColor = this.Mixer.mix(this._baseColor, mixColor, weight);
-    return newColor.colorString();
-  },
-  _determineMixColor: function(doTint) {
-    if (this.mixPure()) {
-      return this._returnPure(doTint);
-    }
-
-    var rgb = this._baseColor.vals();
-    var mixColoreColor = {};
-
-    if (doTint) {
-      mixColoreColor = this._determineTintColor(rgb);
-    } else { // doShade
-      mixColoreColor = this._determineShadeColor(rgb);
-    }
-    return mixColoreColor;
-  },
-  _determineTintColor: function(srcRgb) {
-    var highestVal = Math.max(srcRgb.r, srcRgb.g, srcRgb.b);
-    var minVal = this._clampVal(highestVal / 2, 1, 255);
-
-    var aggregateVal = this._baseColor.aggregateValue(); // Max Value: 765
-    var diffToMax = this._clampVal(765 / aggregateVal, 2, 1000); // min 2 in case baseColor is very bright    
-
-    var rgb = {
-      r: this._clampVal(srcRgb.r, minVal, 255),
-      g: this._clampVal(srcRgb.g, minVal, 255),
-      b: this._clampVal(srcRgb.b, minVal, 255)
-    };
-    var tintColor = new ColoreColor({
-      vals: {
-        r: this._clampVal(Math.floor(rgb.r * diffToMax)),
-        g: this._clampVal(Math.floor(rgb.g * diffToMax)),
-        b: this._clampVal(Math.floor(rgb.b * diffToMax))
-      }
-    });
-
-    return tintColor;
-  },
-  _determineShadeColor: function(srcRgb) {
-    // TODO make smarter.
-    var aggregateVal = this._baseColor.aggregateValue(); // Max Value: 765
-    var diffToMin = this._clampVal(1 / aggregateVal, 0.01, 0.05);
-    var shadeColor = new ColoreColor({
-      vals : {
-        r: Math.floor(srcRgb.r * diffToMin),
-        g: Math.floor(srcRgb.g * diffToMin),
-        b: Math.floor(srcRgb.b * diffToMin)
-      }
-    });
-
-    return shadeColor;
-  },
-  _returnPure: function(doTint) {
-    if (doTint) {
-      return this._white;
-    } else {
-      return this._black;
-    }
-  },
-  _clampVal: function(toClamp, min, max) {
-    // if min/max not provided, assume we are clamping an rgb value.
-    var clampMin = min || 0;
-    var clampMax = max || 255;
-    return Math.min(Math.max(toClamp, clampMin), clampMax);
+    return this.Utils.shiftColor(this._baseColor, percentage, this.mixPure(), false);
   }
 };
+
+
+
+
+var Colore = (function() {
+  var ColoreUtils = (function() {
+    var White = new ColoreColor({
+      vals: { r: 255, g: 255, b: 255 }
+    });
+    var Black = new ColoreColor({
+      vals: { r:   0, g:   0, b:   0 }
+    });
+    return {
+      mix: function(baseColor, mixColor, weight) {
+        var baseRgb = baseColor.vals();
+        var mixRgb = mixColor.vals();
+        var resultRgb = {};
+
+        var alpha = baseColor.alpha(); // Use baseColor's alpha setting.
+
+        for (var c in baseRgb) {
+          if (baseRgb.hasOwnProperty(c)) {
+            var baseVal = baseRgb[c];
+            var mixVal = mixRgb[c];
+            resultRgb[c] = Math.floor(baseVal + (mixVal - baseVal) * (weight / 100.0));
+            resultRgb[c] = this.clampVal(resultRgb[c]);
+          }
+        }
+        var resultColor = new ColoreColor({
+          vals: resultRgb,
+          alpha: alpha
+        });
+
+        return resultColor;
+      },
+      shiftColor: function(baseColor, percentage, mixPure, doTint) {
+        var weight;
+        if (typeof percentage === 'undefined') {
+          weight = 25; // Default of 25% weight
+        } else {
+          weight = (typeof percentage === 'string') ? parseInt(percentage, 10) : percentage;
+        }
+
+        var mixColor = this.determineMixColor(baseColor, mixPure, doTint);
+        var newColor = Colore.Utils.mix(baseColor, mixColor, weight);
+        return newColor.colorString();
+      },
+      determineMixColor: function(baseColor, mixPure, doTint) {
+        if (mixPure) {
+          return this.returnPure(doTint);
+        }
+
+        var mixColoreColor = {};
+        if (doTint) {
+          mixColoreColor = this.determineTintColor(baseColor);
+        } else { // doShade
+          mixColoreColor = this.determineShadeColor(baseColor);
+        }
+        return mixColoreColor;
+      },
+      determineTintColor: function(baseColor) {
+        var srcRgb = baseColor.vals();
+        var highestVal = Math.max(srcRgb.r, srcRgb.g, srcRgb.b);
+        var minVal = this.clampVal(highestVal / 2, 1, 255);
+
+        var aggregateVal = baseColor.aggregateValue(); // Max Value: 765
+        var diffToMax = this.clampVal(765 / aggregateVal, 2, 1000); // min 2 in case baseColor is very bright    
+
+        var rgb = {
+          r: this.clampVal(srcRgb.r, minVal, 255),
+          g: this.clampVal(srcRgb.g, minVal, 255),
+          b: this.clampVal(srcRgb.b, minVal, 255)
+        };
+        var tintColor = new ColoreColor({
+          vals: {
+            r: this.clampVal(Math.floor(rgb.r * diffToMax)),
+            g: this.clampVal(Math.floor(rgb.g * diffToMax)),
+            b: this.clampVal(Math.floor(rgb.b * diffToMax))
+          }
+        });
+
+        return tintColor;
+      },
+      determineShadeColor: function(baseColor) {
+        // TODO make smarter.
+        var srcRgb = baseColor.vals();
+        var aggregateVal = baseColor.aggregateValue(); // Max Value: 765
+        var diffToMin = this.clampVal(1 / aggregateVal, 0.01, 0.05);
+        var shadeColor = new ColoreColor({
+          vals : {
+            r: Math.floor(srcRgb.r * diffToMin),
+            g: Math.floor(srcRgb.g * diffToMin),
+            b: Math.floor(srcRgb.b * diffToMin)
+          }
+        });
+
+        return shadeColor;
+      },
+      returnPure: function(doTint) {
+        if (doTint) {
+          return White;
+        } else {
+          return Black;
+        }
+      },
+      clampVal: function(toClamp, min, max) {
+        // if min/max not provided, assume we are clamping an rgb value.
+        var clampMin = min || 0;
+        var clampMax = max || 255;
+        return Math.min(Math.max(toClamp, clampMin), clampMax);
+      },
+    }
+  })();
+
+  return {
+    Utils: ColoreUtils,
+    mix: function(baseColor, mixColor, weight, opt_asObject) {
+      var resultColor = this.Utils.mix(baseColor, mixColor, weight);
+      if (opt_asObject) {
+        return resultColor;
+      } else {
+        return resultColor.colorString();
+      }
+    },
+    color: function(colorString) {
+      return new ColoreColor({ color: colorString });
+    },
+    getAdjuster: function(options) {
+      return new ColoreAdjuster(options);
+    }
+  }
+})();
+
